ccdd348ee0e4834e821f7f7e3fa7082526dc38e3
leandro silveira <leandro.silveira@squadra.com.br>
2019-10-01 15:32:19 -0300

    data

--- src/data/data.vue
+++ src/data/data.vue
@@ -3,15 +3,42 @@
 export default 
 ﻿[
   {
-      "pergunta": "Dando foco a metodologia Agil, especificamente no que se refere ao framework SCRUM, tem-se alguns ritos a serem seguidos pelo time, tais como Planning, Daily, Retrospectiva, entre outros. Nos concentrando no rito da Retrospectiva quais são as perguntas que os membros da equipe devem responder neste rito para se ter uma melhor analise da Iteração? (Hilderaldo Barbosa)",
+      "pergunta": "O ASP.NET Core é uma tecnologia: (Rodrigo Reis)",
       "respostas": [
-          "Qual foi o objetivo da Iteração? Atingimos as metas especificadas? Qual foi o passo mais importante?",
-          "Houve boa interação das partes? O Cliente sentiu importante? Houve atuação destacada de alguém?",
-          "Onde precisamos melhorar? Podemos manter o processo atual? O Cliente teve boa participação?",
-          "Houve êxito na entrega? Houve Comprometimento da Equipe? Desenvolveu-se um MVP aos olhos do cliente?",
-          "O que houve de bom? O que houve de ruim? Sugestões de melhoria do processo?"
+          "Licenciada",
+          "De código aberto",
+          "Obsoleta",
+          "UI"
       ],
-      "respostaCorreta": "O que houve de bom? O que houve de ruim? Sugestões de melhoria do processo?",
+      "respostaCorreta": "De código aberto",
+      "descricaoRespostaCorreta": "",
+      "categoria": 1,
+      "dificuldade": 0,
+      "jogado": false
+  },
+  {
+      "pergunta": "É uma linguagem de consulta de dados desenvolvida e usada pelo Facebook para realizar requisições e entregar informações para aplicações web e mobile desde 2012: (Rodrigo Reis)",
+      "respostas": [
+          "SQL",
+          "GraphQL",
+          "C#",
+          "Javascrit"
+      ],
+      "respostaCorreta": "GraphQL",
+      "descricaoRespostaCorreta": "",
+      "categoria": 1,
+      "dificuldade": 0,
+      "jogado": false
+  },
+  {
+      "pergunta": "É um Design Pattern para resiliencia de microsserviços: (Rodrigo Reis)",
+      "respostas": [
+          "Singleton",
+          "Factory",
+          "Circuit Breaker",
+          "Http"
+      ],
+      "respostaCorreta": "Circuit Breaker",
       "descricaoRespostaCorreta": "",
       "categoria": 1,
       "dificuldade": 0,
